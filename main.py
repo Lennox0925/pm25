c76362b446ece4bc9f60290647a01281a9fd03ba
lennox0925 <lennox0925@gmail.com>
2025-08-22 20:13:05 +0800

    完成BMI計算MAIN

--- main.py
+++ main.py
@@ -1,75 +1,43 @@
-import pymysql
-import requests
+from flask import Flask
+from datetime import datetime
 
-table_str="""
-create table if not exists pm25(
-id int auto_increment primary key,
-site varchar(25),
-county varchar(50),
-pm25 int,
-datacreationdate datetime,
-itemunit varchar(20),
-unique key site_time (site,datacreationdate)
-)
-"""
+app=Flask(__name__)
 
-url="https://data.moenv.gov.tw/api/v2/aqx_p_02?api_key=540e2ca4-41e1-4186-8497-fdd67024ac44&limit=1000&sort=datacreationdate%20desc&format=JSON"
+@app.route("/bmi/height=<h>&weight=<w>")
+def get_bmi(h,w):
+    bmi=round(eval(w)/(eval(h)/100)**2,2)
+    return f"<h1>身高:{h}cm 體重:{w}Kg<br> BMI:{bmi}</h1>"
 
-sqlstr="insert ignore into pm25(site,county,pm25,datacreationdate,itemunit)\
-      values(%s,%s,%s,%s,%s)"
 
-conn,cursor=None,None
-
-def open_db():
-    global conn,cursor
-    
+@app.route("/books")
+@app.route("/books/id=<int:id>")
+def get_book(id=None):
     try:
-        conn=pymysql.connect(
-        host="localhost",
-        user="root",
-        password="",
-        port=3307,
-        database="demo"
-        )
+        books = {1:"Python book",2: "Java book", 3: "Flask book"}
 
-        print(conn)
-        cursor=conn.cursor()
-        print("資料庫開啟成功!!")
+        if id==None:
+            return books
+
+        return books[id]
     except Exception as e:
-        print(e)
+        return f"書籍編號錯誤:{e}"
 
 
-def close_db():
-    if conn is not None:
-        conn.close()
-        print("資料庫關閉成功!!")
+@app.route("/nowtime")
+def now_time():
+    time=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+   # print(time)
+    return time
 
 
-def get_open_data():
-    resp=requests.get(url,verify=False)
-    datas=resp.json()['records']
-    values=[list(data.values()) for data in datas if list(data.values())[2]!=""]
-    return values
-
-def write_to_data():
-    try:
-        values=get_open_data()
-        if len(values)==0:
-            print("目前無資料")
-            return
-        
-        
-        size=cursor.executemany(sqlstr,values)
-        conn.commit()
-        print(f"寫入{size}資料成功")
-    except Exception as e:
-        print(e)
+@app.route("/")
+def index():
+    return "<h1>Hello Lennox!!</h1>"
 
 
 
 
-open_db()
-write_to_data()
-close_db()
 
-#print(get_open_data())
+app.run(debug=True)
+
+
